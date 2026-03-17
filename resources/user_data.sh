559e9dcf813f8d3b69c3f99addeb669e7e030301
reginabianchini <regina.bianchinidemirandadamatta@student.saultcollege.ca>
2026-03-17 15:51:31 +0000

    lab continuation

--- resources/user_data.sh
+++ resources/user_data.sh
@@ -16,7 +16,7 @@ APP_DIR=/home/ec2-user/dice
 mkdir -p $APP_DIR
 
 # Clone your application repository (replace with your repo URL)
-git clone !!!Your clone URL here!!! $APP_DIR
+git clone https://github.com/saultcollege-csd215/aws-project-26w-reginabianchini.git  $APP_DIR
 
 # Set the owner of the repo folder to be the user account that will ultimately run the app
 chown -R ec2-user:ec2-user $APP_DIR
